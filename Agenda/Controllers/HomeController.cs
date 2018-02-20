ddf70b983d49f5ab72bcbec815559bf3a3590e49
aluno <aluno@labivj.info>
2018-02-20 20:20:34 -0300

    Inserindo informações no sistema

---
+++ Agenda/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Agenda.Model.DB.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agenda.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {          
+            return View(Pessoa.Pessoas);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult PersistPearson(Pessoa p)
+        {
+            Pessoa.Pessoas.Add(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
