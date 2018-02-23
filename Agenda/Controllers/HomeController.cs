ca92a2907fad6d21fc70efe73c18f1280c0720bb
aluno <aluno@labivj.info>
2018-02-22 22:10:56 -0300

    implementando a busca

--- Agenda/Controllers/HomeController.cs
+++ Agenda/Controllers/HomeController.cs
@@ -12,7 +12,35 @@ namespace Agenda.Controllers
     {
         // GET: Home
         public ActionResult Index()
-        {          
+        {
+            var p1 = new Pessoa()
+            {
+                Nome="Tadeu",
+                Telefone ="32 32323232",
+                Email = "email@oi.com.br",
+                DtNascimento = DateTime.Now,
+                Id = Guid.NewGuid()
+            };
+            var p2 = new Pessoa()
+            {
+                Nome = "B",
+                Telefone = "32 32323232",
+                Email = "email@oi.com.br",
+                DtNascimento = DateTime.Now,
+                Id = Guid.NewGuid()
+            };
+            var p3 = new Pessoa()
+            {
+                Nome = "C",
+                Telefone = "32 32323232",
+                Email = "email@oi.com.br",
+                DtNascimento = DateTime.Now,
+                Id = Guid.NewGuid()
+            };
+
+            Pessoa.Pessoas.Add(p1);
+            Pessoa.Pessoas.Add(p2);
+            Pessoa.Pessoas.Add(p3);
             return View(Pessoa.Pessoas);
         }
 
@@ -25,6 +53,46 @@ namespace Agenda.Controllers
         {
             Pessoa.Pessoas.Add(p);
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Search(String edtSearch)
+        {
+            var pessoaAux = new List<Pessoa>();
+            if (String.IsNullOrEmpty(edtSearch))
+            {
+                return View("Index", Pessoa.Pessoas);
+            }
+            //1 - Filtro com FOR
+            //for(var i=0; i<Pessoa.Pessoas.Count; i++)
+            //{
+            //    if(Pessoa.Pessoas[i].Nome == edtSearch.Trim())
+            //    {
+            //        pessoaAux.Add(Pessoa.Pessoas[i]);
+            //    }
+            //}
+
+            //2 - Filtro com FOREACH
+            //foreach(var pessoa in Pessoa.Pessoas)
+            //{
+            //    if(pessoa.Nome == edtSearch.Trim())
+            //    {
+            //        pessoaAux.Add(pessoa);
+            //    }
+            //}
+
+            //3 - Filtro com LINQ
+            //pessoaAux = (
+            //    from p in Pessoa.Pessoas
+            //    where p.Nome == edtSearch.Trim()
+            //    select p
+            //    ).ToList();
+
+            //4 - Filtro com LAMBDA
+            pessoaAux = Pessoa.Pessoas.Where(pessoa =>  
+                                pessoa.Nome.Contains(edtSearch.Trim())
+                            ).ToList();
+
+            return View("Index", pessoaAux);
         }
     }
 }
