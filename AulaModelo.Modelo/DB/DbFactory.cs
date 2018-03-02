08dadc9b04453905f444dff15ef35cc22f02cdc8
aluno <aluno@labivj.info>
2018-03-01 22:24:00 -0300

    Inserindo o NHibernate ao projeto

---
+++ AulaModelo.Modelo/DB/DbFactory.cs
@@ -0,0 +1,76 @@
+﻿using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaModelo.Modelo.DB
+{
+    public class DbFactory
+    {
+        private static DbFactory _instance = null;
+
+        private DbFactory()
+        {
+            
+        }
+
+        public static DbFactory Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new DbFactory();
+                }
+
+                return _instance;
+            }
+        }
+
+        private void Connection()
+        {
+            try
+            {
+                var stringConnection = "";
+                ConfigureNHibernate(stringConnection);
+            }catch(Exception ex)
+            {
+                throw new Exception("Cannot connect to database. Error: ", ex);
+            }
+        }
+
+        private void ConfigureNHibernate(string stringConnection)
+        {
+            try
+            {
+                //using NHibernate.cfg
+                var config = new Configuration();
+
+                //Configuracao do NHibernate com o MySQL
+                config.DataBaseIntegration(i =>
+                {
+                    //Dialeto do banco
+                    i.Dialect<NHibernate.Dialect.MySQLDialect>();
+                    //Conexao string
+                    i.ConnectionString = stringConnection;
+                    //Drive de conexao com o banco
+                    i.Driver<NHibernate.Driver.MySqlDataDriver>();
+                    //Provedor de conexao do MySQL
+                    i.ConnectionProvider<NHibernate.Connection.DriverConnectionProvider>();
+                    //Gera log dos sql executados no console
+                    i.LogSqlInConsole = true;
+                    //descomentar caso queira visualizar o log de sql formatado no console
+                    i.LogFormattedSql = true;
+                    //cria schema do banco de dados sempre que a configuration for utilizada
+                    i.SchemaAction = SchemaAutoAction.Update;
+                });
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Cannot configure NHibernate. Error: ", ex);
+            }
+        }
+    }
+}
--- AulaModelo.Modelo/DB/Model/Pessoa.cs
+++ AulaModelo.Modelo/DB/Model/Pessoa.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +12,22 @@ namespace AulaModelo.Modelo.DB.Model
     {
         public static List<Pessoa> Pessoas = new List<Pessoa>();
 
-        public Guid Id { get; set; }
-        public String Nome { get; set; }
-        public DateTime DtNascimento { get; set; }
-        public String Email { get; set; }
-        public String Telefone { get; set; }
+        public virtual Guid Id { get; set; }
+        public virtual String Nome { get; set; }
+        public virtual DateTime DtNascimento { get; set; }
+        public virtual String Email { get; set; }
+        public virtual String Telefone { get; set; }
+    }
+
+    public class PessoaMap : ClassMapping<Pessoa>
+    {
+        public PessoaMap()
+        {
+            Id(x => x.Id, m => m.Generator(Generators.Guid));
+            Property(x => x.Nome);
+            Property(x => x.DtNascimento);
+            Property(x => x.Email);
+            Property(x => x.Telefone);
+        }
     }
 }
