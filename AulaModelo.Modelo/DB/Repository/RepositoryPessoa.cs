adc0ac9a6787ef1fbb1bde6a12881232b8511f44
aluno <aluno@labivj.info>
2018-03-08 21:53:51 -0300

    criando classes Repository

---
+++ AulaModelo.Modelo/DB/Repository/RepositoryPessoa.cs
@@ -0,0 +1,17 @@
+﻿using AulaModelo.Modelo.DB.Model;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaModelo.Modelo.DB.Repository
+{
+    public class RepositoryPessoa : RepositoryBase<Pessoa>
+    {
+        public RepositoryPessoa(ISession session) : base(session)
+        {
+        }
+    }
+}
